1770ea556552ee4fbeb501ddf0fbdea9ca9bfc69
Jugal Panchal <jugalpanchal@gmail.com>
2017-03-23 07:38:00 +0530

    Implemented progressbar with background worker

--- WPFSamples/ProgressBarWithBackgroundWorker/MainWindow.xaml.cs
+++ WPFSamples/ProgressBarWithBackgroundWorker/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +22,47 @@ namespace ProgressBarWithBackgroundWorker
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BackgroundWorker backgroundWorker;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.backgroundWorker = new BackgroundWorker();
+            this.backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            this.backgroundWorker.ProgressChanged += BackgroundWorker_ProgressChanged;
+            this.backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+        }
+
+        void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //NOTE : Never play with the UI thread here.
+            int length = Convert.ToInt32(e.Argument);
+            int progressValue = 0; 
+            while(progressValue < length)
+            {
+                Thread.Sleep(100);
+                progressValue = progressValue + 10;
+                backgroundWorker.ReportProgress(progressValue);
+            }
+        }
+
+        void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //Here play with the main UI thread
+            this.ProgressBar.Value = e.ProgressPercentage;
+        }
+
+        void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.backgroundWorker.RunWorkerAsync(100);
         }
     }
 }
